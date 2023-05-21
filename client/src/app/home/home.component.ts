a23f7137477d770b1bb0e623458846e4a8a1bf4c
Ricudo <coolcherry281@gmail.com>
2023-05-21 22:57:29 +0200

    Removed empty style files

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -4,7 +4,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -4,7 +4,6 @@ import { AccountService } from '../_services/account.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
