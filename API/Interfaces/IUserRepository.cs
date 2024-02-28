da7cb13972c99bbb17340cc459deeef2080e4a25
Ricudo <coolcherry281@gmail.com>
2024-02-29 00:07:24 +0100

    (API) query optimisation and chores

--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -4,10 +4,10 @@ using API.Helpers;
 public interface IUserRepository
 {
     void Update(AppUser user);
-    Task<bool> SaveAllAsync();
     Task<IEnumerable<AppUser>> GetUsersAsync();
     Task<AppUser> GetUserByIdAsync(int id);
     Task<AppUser> GetUserByUsernameAsync(string username);
     Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams);
-    Task<MemberDto> GetMemberAsync(string userName);
+    Task<MemberDto> GetMemberAsync(string username);
+    Task<string> GetUserGenderAsync(string username);
 }
