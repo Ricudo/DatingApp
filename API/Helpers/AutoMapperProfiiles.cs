85cd0f53f90eedcf0a32565c117ce040cc14fb41
Ricudo <coolcherry281@gmail.com>
2024-02-28 21:30:04 +0100

    fixed DateTime issue

--- API/Helpers/AutoMapperProfiiles.cs
+++ API/Helpers/AutoMapperProfiiles.cs
@@ -21,6 +21,8 @@ namespace API.Helpers
                     opt => opt.MapFrom(s => s.Sender.Photos.FirstOrDefault(x => x.IsMain).Url))
                 .ForMember(d => d.RecipientPhotoUrl,
                     opt => opt.MapFrom(s => s.Recipient.Photos.FirstOrDefault(x => x.IsMain).Url));
+            CreateMap<DateTime, DateTime>().ConvertUsing(d => DateTime.SpecifyKind(d, DateTimeKind.Utc));
+            CreateMap<DateTime?, DateTime?>().ConvertUsing(d => d.HasValue ? DateTime.SpecifyKind(d.Value, DateTimeKind.Utc) : null);
         }
     }
 }
