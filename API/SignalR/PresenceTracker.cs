4c5381a263651c23155ed417f7e5b06502110a45
Ricudo <coolcherry281@gmail.com>
2024-02-28 21:59:48 +0100

    Add messages notifications

--- API/SignalR/PresenceTracker.cs
+++ API/SignalR/PresenceTracker.cs
@@ -52,5 +52,17 @@ namespace API.SignalR
 
             return Task.FromResult(onlineUsers);
         }
+
+        public static Task<List<string>> GetConnectionsForUser(string username)
+        {
+            List<string> connectionsIds;
+
+            lock (OnlineUsers)
+            {
+                connectionsIds = OnlineUsers.GetValueOrDefault(username);
+            }
+
+            return Task.FromResult(connectionsIds);
+        }
     }
 }
--- client/src/app/_services/presence.service.ts
+++ client/src/app/_services/presence.service.ts
@@ -3,7 +3,8 @@ import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { ToastrService } from 'ngx-toastr';
 import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, take } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -14,7 +15,7 @@ export class PresenceService {
   private _onlineUsersSource$ = new BehaviorSubject<string[]>([]);
   onlineUsers$ = this._onlineUsersSource$.asObservable();
 
-  constructor(private _toastr: ToastrService) {}
+  constructor(private _toastr: ToastrService, private _router: Router) {}
 
   createHubConnection(user: User) {
     this._hubConnection = new HubConnectionBuilder()
@@ -36,6 +37,18 @@ export class PresenceService {
 
     this._hubConnection.on('GetOnlineUsers', (usernames) => {
       this._onlineUsersSource$.next(usernames);
+    });
+
+    this._hubConnection.on('NewMessageReceived', ({ username, knownAs }) => {
+      this._toastr
+        .info(knownAs + ' has sent you a new message! Click me to see it')
+        .onTap.pipe(take(1))
+        .subscribe({
+          next: () =>
+            this._router.navigateByUrl(
+              '/members/' + username + '?tab=Messages'
+            ),
+        });
     });
   }
 
